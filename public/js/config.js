806a8e42e3eac67365cff735d05c2a635a4e2a62
chompoo500ml <chompoo@gang500.ml>
2018-04-20 13:13:32 +0000

    update-heroku-url

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 function getEndpointArticle(){
   //return "https://safe-coast-54130.herokuapp.com/api/v1/articles";
-  return "https://aqueous-sea-58052.herokuapp.com/api/v2/articles";
+  return "https://powerful-garden-18745.herokuapp.com/api/v2/articles";
 }
